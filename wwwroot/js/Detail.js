0af085b0f199679eca65075193169adf600eddda
Mattd440 <mmdiederick@gmail.com>
2017-11-01 14:45:55 -0400

    Update Detail.js
    added file downloading

--- wwwroot/js/Detail.js
+++ wwwroot/js/Detail.js
@@ -1,6 +1,23 @@
-﻿function GetFile()
+
+function GetFile()
 {
+    var blob;
+    var data;
+    var a = document.getElementById("a");
+    var txtFile = "test.txt";
+
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', '/Saved/DownloadFile', true);
-    xhr.send();
+    xhr.open('POST', '/Saved/GetNetworkFile', true);
+    xhr.onreadystatechange=function() {
+     if (xhr.readyState==4) {
+        data = (xhr.responseText);
+        console.log(data);
+        blob = new Blob([data],{type: "text/plain;charset=utf-8"});
+        console.log(blob);
+        saveAs(blob,txtFile);
+      }
+     }
+     xhr.send(null);
+       
+
 }
